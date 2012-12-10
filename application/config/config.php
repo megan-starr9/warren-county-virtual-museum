381a5f9d2abfc792e8d8056c7be3ba550aeb69c7
Paul Schuytema <paul.schuytema@cityofmonmouth.com>
2012-12-10 11:04:50 -0600

    added in bitAuth user library

--- application/config/config.php
+++ application/config/config.php
@@ -225,7 +225,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '45w0PRH68suRsak99w314454el7hdjEg';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'warcovrt';
+$db['default']['password'] = 'virtual12';
+$db['default']['database'] = 'warcovrt_vm';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
