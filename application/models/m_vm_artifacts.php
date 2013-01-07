2efc3aa7f2d6757f4c3c2c441804d72474bfd963
Megan Starr <megan.starr9@gmail.com>
2013-01-07 16:31:58 -0600

    Small updates to artifact model and curator's edit artifact view

--- application/models/m_vm_artifacts.php
+++ application/models/m_vm_artifacts.php
@@ -21,16 +21,18 @@ class m_vm_artifacts extends CI_Model{
         //not done!!!
         $data = array(
             'Title' => $this->pcs_utility->db_clean(strip_tags($this->input->post('Title')),100),
-            'Org' => $this->pcs_utility->db_clean(strip_tags($this->input->post('Org')),100)
+            'Org' => $this->pcs_utility->db_clean(strip_tags($this->input->post('Org')),100),
+        	'Description' => $this->pcs_utility->db_clean(strip_tags($this->input->post('Description'))),
+        		// 0 for now, must find user session data
+        	'Creator_Id' => 0
         );
         $this->db->set($data);
         if (isset($artifact_id))
         {
-            
             $this->db->where('PK_Artifact_Id',$this->pcs_utility->id_clean($artifact_id));
-            $this->db->update('vm_artifacts');
+            $this->db->update('vm_artifacts', $data);
         } else {
-            $this->db->insert('vm_artifacts');
+            $this->db->insert('vm_artifacts', $data);
         }
 
     }
@@ -40,10 +42,11 @@ class m_vm_artifacts extends CI_Model{
         if(isset($artifact_id))
         {
             $this->db->where('PK_Artifact_Id', $artifact_id);
+            $this->db->limit(1);
             $this->db->delete('vm_artifacts');
         }
         //@todo
-        //remove all edia objects as well (or leave hanging?)
+        //remove all media objects as well (or leave hanging?)
     }
     
     function get_artifacts()
--- application/views/curate_artifact_update.php
+++ application/views/curate_artifact_update.php
@@ -5,14 +5,30 @@
                 echo '<h4>'.$page.'</h4>';
                 if($this->bitauth->has_role('can_edit_artifacts'))
                 {
+                	// Set values for form if editing existing artifact
+                	if(isset($artifact_info))
+                	{
+                		$form_title = $artifact_info['Title'];
+                		$form_org = $artifact_info['Org'];
+                		$form_desc = $artifact_info['Description'];
+                	} else {
+                		
+						$form_title = '';
+						$form_org = '';
+						$form_desc = '';
+					}
                     ?>
                 
                         <form action="<?php echo current_url(); ?>" method="post">
                             <fieldset>
                                 <label>Title</label>
-                                <input type="text" name="Title" class="input-xlarge"><br>
+                                <input type="text" name="Title" class="input-xlarge" value="<?php echo $form_title; ?>"><br>
                                 <label>Organization</label>
+                                <!-- Todo: Set existing artifact value -->
                                 <?php echo form_dropdown('Org', $this->config->item('orgs')); ?>
+         						<br>
+                                <label>Description</label>
+                                <textarea name="Description"><?php echo $form_desc; ?></textarea>
                                 <br>
                                 <button type="submit" class="btn">Submit</button>
                             </fieldset>
